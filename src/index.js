233e279744cb0997b2e19736449cc262589a27f4
vzdrizhni <vzdrizhni@gmail.com>
2020-08-01 17:55:35 +0300

    Linters errors fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import 'phaser';
-import WorldScene from './scenes/worldScene.js'
-import GameOverScene from './scenes/gameOverScene.js'
-import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin'
-import EnterNameScene from './scenes/enterNameScene.js'
-import TitleScene from './scenes/titleScene.js'
-
-let game
+import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin';
+import WorldScene from './scenes/worldScene';
+import GameOverScene from './scenes/gameOverScene';
+import EnterNameScene from './scenes/enterNameScene';
+import TitleScene from './scenes/titleScene';
 
 window.onload = () => {
-  var config = {
-    type: Phaser.AUTO,
+  const config = {
+    type: Phaser.AUTO, // eslint-disable-line no-undef
     parent: 'content',
     width: 320,
     height: 240,
@@ -19,29 +17,29 @@ window.onload = () => {
       default: 'arcade',
       arcade: {
         gravity: {
-          y: 0
+          y: 0,
         },
-      }
+      },
     },
     scene: [
       TitleScene,
       EnterNameScene,
       WorldScene,
-      GameOverScene      
+      GameOverScene,
     ],
     dom: {
-      createContainer: true
+      createContainer: true,
     },
     plugins: {
       scene: [{
         key: 'rexUI',
         plugin: RexUIPlugin,
-        mapping: 'rexUI'
-      }]
-    }
+        mapping: 'rexUI',
+      }],
+    },
   };
 
-  Phaser.Game.prototype.score = 0;
-  Phaser.Game.prototype.playerName = "";
-  game = new Phaser.Game(config);
-}
+  Phaser.Game.prototype.score = 0; // eslint-disable-line no-undef
+  Phaser.Game.prototype.playerName = ''; // eslint-disable-line no-undef
+  new Phaser.Game(config); // eslint-disable-line no-undef, no-new, no-undef
+};
--- src/scenes/gameOverScene.js
+++ src/scenes/gameOverScene.js
@@ -1,76 +1,71 @@
-import TweenHelper from '../helperClasses/tweenHelper.js'
+import TweenHelper from '../helperClasses/tweenHelper';
 
-class GameOverScene extends Phaser.Scene {
-    constructor() {
-        super('GameOverScene');
-    }
+class GameOverScene extends Phaser.Scene { // eslint-disable-line no-undef
+  constructor() {
+    super('GameOverScene');
+  }
 
-    async create() {
+  async create() {
+    this.result = await this.gameData();
+    this.result = this.result.result;
 
-        this.result = await this.gameData();
-        this.result = this.result.result;
+    this.text = this.add.text(0, 0, `Score: ${this.game.score}`, {
+      font: '16px Arial',
+      fill: '#ffffff',
+      align: 'center',
+    });
 
-        this.text = this.add.text(0, 0, `Score: ${this.game.score}`, {
-            font: "16px Arial",
-            fill: "#ffffff",
-            align: "center"
-        });
+    this.textName = this.add.text(0, 20, `Name: ${this.game.playerName}`, {
+      font: '16px Arial',
+      fill: '#ffffff',
+      align: 'center',
+    });
 
-        this.textName = this.add.text(0, 20, `Name: ${this.game.playerName}`, {
-            font: "16px Arial",
-            fill: "#ffffff",
-            align: "center"
-        });
+    this.textRestart = this.add.text(this.cameras.main.width / 4 + 10, this.cameras.main.width / 2 + 20, 'Press space to restart', {
+      font: '16px Arial',
+      fill: '#ffffff',
+      align: 'center',
+    });
 
-        this.textRestart = this.add.text(this.cameras.main.width / 4 + 10, this.cameras.main.width / 2 + 20, `Press space to restart`, {
-            font: "16px Arial",
-            fill: "#ffffff",
-            align: "center"
-        });        
+    this.add.text(this.cameras.main.width / 4 + 20, this.cameras.main.width / 4 - 20, 'Leader Board:', {
+      color: 'white',
+      fontSize: '16px ',
+    });
 
-        this.add.text(this.cameras.main.width / 4 + 20, this.cameras.main.width / 4 - 20, 'Leader Board:', {
+    this.result.sort((x, y) => y.score - x.score);
+    for (let i = 0; i < 5; i += 1) {
+      if (this.result[i] !== undefined) {
+        this.add.text(this.cameras.main.width / 4, this.cameras.main.width / 4 + (12 * i),
+          `${i + 1}. ${this.result[i].user} -- ${this.result[i].score}`, {
             color: 'white',
             fontSize: '16px ',
-        });
-
-        this.result.sort((x, y) => y.score - x.score);
-        for (let i = 0; i < 5; i += 1) {
-            if (this.result[i] !== undefined) {
-                this.add.text(this.cameras.main.width / 4, this.cameras.main.width / 4 + (12 * i),
-                    `${i + 1}. ${this.result[i].user} -- ${this.result[i].score}`, {
-                        color: 'white',
-                        fontSize: '16px '
-                    });
-            }
-        };
-
-        TweenHelper.flashElement(this, this.textRestart);
-    };
-
-    update(time, delta) {
-        this.cursor = this.input.keyboard.createCursorKeys();
-        if (this.cursor.space.isDown) {
-            this.game.score = 0;
-            this.scene.start("WorldScene");
-        }
+          });
+      }
     }
 
-    async gameData() {
-        const url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/UWGWN7rrI8dxg3RoMxko/scores/";
+    TweenHelper.flashElement(this, this.textRestart);
+  }
 
-        return fetch(url, {
-            method: "GET",
-            mode: 'cors',
-            headers: {
-                Accept: 'Application/json',
-                'Content-Type': 'application/json',
-            },
-        }).then(response => {
-            return response.json()
-        }).then(content => content);
+  update() {
+    this.cursor = this.input.keyboard.createCursorKeys();
+    if (this.cursor.space.isDown) {
+      this.game.score = 0;
+      this.scene.start('WorldScene');
     }
+  }
 
+  async gameData() { // eslint-disable-line class-methods-use-this
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/UWGWN7rrI8dxg3RoMxko/scores/';
 
+    return fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        Accept: 'Application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json()).then(content => content);
+  }
 }
 
-export default GameOverScene
+export default GameOverScene;
